28717b29cb4bfffade3c7e396c20fa1a1f6f6d15
phgodoy <phgodoy12@gmail.com>
2025-12-28 00:02:00 -0300

    05 vec exercise solveds

--- exercises/05_vecs/vecs2.rs
+++ exercises/05_vecs/vecs2.rs
@@ -2,6 +2,7 @@ fn vec_loop(input: &[i32]) -> Vec<i32> {
     let mut output = Vec::new();
 
     for element in input {
+         output.push(element * 2);
         // TODO: Multiply each element in the `input` slice by 2 and push it to
         // the `output` vector.
     }
@@ -25,6 +26,7 @@ fn vec_map(input: &[i32]) -> Vec<i32> {
         .iter()
         .map(|element| {
             // ???
+            element * 2
         })
         .collect()
 }
